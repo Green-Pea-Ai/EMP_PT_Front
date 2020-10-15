db019d8b7ce2f602345118018c3b7fa87b0501a6
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-10-15 15:35:06 +0900

    목요일, 추가 커밋

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as Home } from './Home'
 export { default as Board } from './Board';
 export { default as Cheese } from './Cheese';
-export { default as Main } from '../Main';
 export { default as Survey } from './Survey';
 export { default as User } from './User';
+
+// export { default as Main } from '../Main';
